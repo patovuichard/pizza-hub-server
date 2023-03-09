dc4646b43245d1e0d2f3b9cc2fb05e8d553532b1
Patricio Sanchez Vuichard <patriciosanchezvuichard@gmail.com>
2023-03-09 20:38:32 +0100

    auth workin fine

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,10 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-const authRoutes = require("./auth.routes.js")
-router.use("/auth", authRoutes)
+const authRoutes = require("./auth.routes.js");
+router.use("/auth", authRoutes);
+
+const pizzaRoutes = require("./pizza.routes.js");
+router.use("/pizza", pizzaRoutes);
 
 module.exports = router;
